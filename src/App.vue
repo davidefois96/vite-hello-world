5369a2f6fa0d372c0d9f95ceb820c88da8554972
davidefois96 <davidefois360@gmail.com>
2024-03-21 01:37:21 +0100

    [add] vite-hello-world full code

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default{
+
+  data(){
+
+    return{
+
+      msg:'hello world'
+
+    }
+
+  }
+
+
+}
+
+
+</script>
+
+<template>
+
+  <div>
+
+    <h1>{{ msg }}</h1>
+    
+  </div>
+ 
+</template>
+
+<style>
+
+</style>
